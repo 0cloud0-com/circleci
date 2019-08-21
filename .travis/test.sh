337cf61416c4143de55829efc1d42a26777f7d64
aninath <aninath@cisco.com>
2019-08-21 17:38:30 +0530

    1.txt

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,7 +1,11 @@
-VERSION=$(echo ROOT.war | md5sum | awk '{ print $1 }')
+VERSION=$(echo /home/travis/build/0cloud0-com/circleci/ROOT.war | md5sum | awk '{ print $1 }')
 
 pwd
 echo ROOT.war | md5sum
+
+echo /home/travis/build/0cloud0-com/circleci/ROOT.war | md5sum
+
+echo "changed"
 
 echo $VERSION
 ls -ltr
